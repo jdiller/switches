42d7ca7bf601132aa716933509ff499bd346c616
Jason Diller <jason@diller.ca>
2018-01-27 11:40:24 -0500

    Some enhancements to make it easier to use with Google Assistant

--- switches.py
+++ switches.py
@@ -12,12 +12,17 @@ logging.basicConfig(level=logging.DEBUG)
 
 PIN = 21
 
-switches = [Switch('Lamp1', 333107, 333116),
-            Switch('Lamp2', 333251, 333260),
+switches = [Switch('Bookshelf', 333107, 333116),
+            Switch('Tube', 333251, 333260),
             Switch('Kitchen', 333571, 333580),
             Switch('Humidifier', 335107, 335116),
-            Switch('Lamp3', 341251, 341260)]
+            Switch('Floor', 341251, 341260)]
 
+switch_groups = {
+        'Living Room': [switches[0], switches[1], switches[4]],
+        'TV' : [switches[0],switches[4]],
+        'All' : [switches[0], switches[1], switches[2], switches[4]]
+}
 
 @app.route("/")
 def home():
@@ -38,6 +43,18 @@ def switch(switch_name, action):
         _send_code(switch.off_code)
     return 'ok'
 
+@app.route('/group/<group_name>/<action>', methods=['POST'])
+def group(group_name, action):
+    switch_group = switch_groups.get(group_name)
+    if not switch_group:
+        abort(404)
+    for switch in switch_group:
+        if action.lower() == 'on':
+            _send_code(switch.on_code)
+        else:
+            _send_code(switch.off_code)
+    return 'ok'
+
 
 def _send_code(code):
     pi = pigpio.pi()
@@ -48,7 +65,8 @@ def _send_code(code):
 
 
 def _find_switch(switch):
-    return next(sw for sw in switches if sw.name == switch)
+    switch = switch.replace('the ', '')
+    return next(sw for sw in switches if lower(sw.name.lower() == switch.lower())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
